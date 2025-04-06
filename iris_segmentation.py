e0d60762882889a28ce5c7c208b8fa0c05cc2f13
sienkozuzanna <zuza.sienko@wp.pl>
2025-04-06 14:43:18 +0200

    iris and pupil binarization

--- iris_segmentation.py
+++ iris_segmentation.py
@@ -6,13 +6,47 @@ import matplotlib.pyplot as plt
 def convert_to_grayscale(image):
     if image is None:
         raise ValueError("Image not found or unable to load.")
+    
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-#binarization for custom threshold
-def binary_threshold(image, threshold=127):
-    if image is None:
+def compute_binarization_threshold(grayscale_image):
+    if grayscale_image is None:
         raise ValueError("Image not found or unable to load.")
-    if threshold < 0 or threshold > 255:
-        raise ValueError("Threshold must be between 0 and 255.")
-    _, binary_image = cv2.threshold(image, threshold, 255, cv2.THRESH_BINARY)
-    return binary_image
+    if len(grayscale_image.shape) != 2:
+        raise ValueError("Image must be a grayscale image.")
+    
+    h, w = grayscale_image.shape
+    total_sum = np.sum(grayscale_image) #sum of all pixel values
+    P = total_sum / (h * w) #mean brightness
+    return P
+
+#X_I from experiments 
+def iris_binarization(grayscale_image, iris_mask, X_I):
+    if grayscale_image is None:
+        raise ValueError("Image not found or unable to load.")
+    if len(grayscale_image.shape) != 2:
+        raise ValueError("Image must be a grayscale image.")
+    
+    P = compute_binarization_threshold(grayscale_image)
+    #binarization threshold for iris
+    P_I = P / X_I 
+    #applying iris mask
+    iris_region = cv2.bitwise_and(grayscale_image, grayscale_image, mask=iris_mask)
+    _, binary_iris = cv2.threshold(iris_region, P_I, 255, cv2.THRESH_BINARY)
+    
+    return binary_iris
+
+#X_P from experiments
+def pupil_binarization(grayscale_image, pupil_mask, X_P):
+    if grayscale_image is None:
+        raise ValueError("Image not found or unable to load.")
+    if len(grayscale_image.shape) != 2:
+        raise ValueError("Image must be a grayscale image.")
+    
+    P = compute_binarization_threshold(grayscale_image)
+    #binarization threshold for pupil
+    P_I = P / X_P
+    #applying pupil mask
+    pupil_region = cv2.bitwise_and(grayscale_image, grayscale_image, mask=pupil_mask)
+    _, binary_pupil = cv2.threshold(pupil_region, P_I, 255, cv2.THRESH_BINARY)
+    return binary_pupil
